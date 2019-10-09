27a45702609dd1e05101b4cc66e5d247f4b9d054
Brad Phillips <bjrp@protonmail.com>
2019-10-09 16:49:45 +0100

    Incomplete implementation of update feature

--- app.rb
+++ app.rb
@@ -30,6 +30,15 @@ class BookmarkManager < Sinatra::Base
     redirect '/bookmarks'
   end
 
+  get '/bookmarks/:id/edit' do
+    @bookmark_id = params[:id]
+    erb :'bookmarks/edit' 
+  end
+
+  patch '/bookmarks/:id' do
+    redirect '/bookmarks'
+  end
+
 
   run! if app_file == $0
 end
---
+++ spec/features/update_bookmark_spec.rb
@@ -0,0 +1,18 @@
+feature 'Updating a bookmark' do
+  scenario 'User can update a bookmark' do
+  bookmark = Bookmarks.create(url: 'http://www.reddt.com', title: 'Reddit')
+  visit('/bookmarks')
+  expect(page).to have_link('Reddit', href: 'http://www.reddt.com')
+
+  first('.bookmark').click_button 'Edit'
+  expect(current_path).to eq "/bookmarks/#{bookmark.id}/edit"
+
+  fill_in('url', with: 'www.reddit.com')
+  fill_in('title', with: 'Reddit')
+  click_button('Submit')
+
+  expect(current_path).to eq '/bookmarks'
+  expect(page).to have_link('Reddit', href: 'http://www.reddit.com')
+  expect(page).not_to have_link('Reddit', href: 'http://www.reddt.com')
+  end
+end
